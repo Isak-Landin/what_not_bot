25cdd6a4445c0be40cc513745a19cebc78ede39c
Isak-Landin <utbisalan@gmail.com>
2022-03-26 01:20:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from pathlib import Path
+import mouse
 
-path = str(Path(__file__).parent.resolve())
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:8001")
+print(mouse)
